636f9b676d39c321daab1f366ffbad1750c5b99b
grvzard <grvzard@outlook.com>
2024-01-13 14:48:01 +0800

    (frontend): inaccurate time related notice, search bar loading effect

--- frontend/src/components/AppLayout.vue
+++ frontend/src/components/AppLayout.vue
@@ -21,11 +21,13 @@ const colorOfStatus = computed(() => {
       return 'bg-gray-400'
   }
 })
+const isLoading = ref(false)
 const is_searchbar_focused = ref(false)
 const search_text = ref('')
 const results_pattern = ref('')
 const search_results: Ref<StreamerInfo[]> = ref([])
 function doSearch(q: string) {
+  isLoading.value = true
   axios({
     method: 'get',
     url: '/search',
@@ -39,10 +41,11 @@ function doSearch(q: string) {
       }
       else {
         onlineStatus.value = onlineStatusE.On
-        console.log(`search(${q}) results: `, resp.data)
+        // console.log(`search(${q}) results: `, resp.data)
         if (q === search_text.value) {
           search_results.value = resp.data
           results_pattern.value = q
+          isLoading.value = false
         }
       }
     })
@@ -71,8 +74,8 @@ function onInput(_event: any) {
       <!-- search bar -->
       <input v-model="search_text" type="text"
         class="p-2 pl-4 mx-6 bg-slate-100 border border-slate-300 grow rounded-md focus:outline-none focus:bg-white hover:bg-white transition-colors"
-        placeholder="uid / 房间号" @input="onInput" @keydown.enter="console.log(`select ${search_text}`)"
-        @focus="is_searchbar_focused = true" @blur="is_searchbar_focused = false" />
+        placeholder="uid / 房间号" @input="onInput" @focus="is_searchbar_focused = true"
+        @blur="is_searchbar_focused = false" />
       <!-- online status -->
       <span class="inline-flex relative h-3 w-3">
         <span class="absolute w-full h-full rounded-full animate-ping opacity-75" :class="colorOfStatus"></span>
@@ -84,18 +87,21 @@ function onInput(_event: any) {
 
   <!-- AppBody -->
   <div class="mx-10 mt-3 overflow-visible relative z-50">
-    <div v-show="is_searchbar_focused" class="absolute w-full flex flex-col shadow-md rounded p-2 bg-white">
-      <span class="pl-3">
-        "{{ results_pattern }}" 搜索结果
-      </span>
-      <button v-for="streamer in search_results" :key="streamer.uid" class="p-2 pl-3 rounded hover:bg-gray-200 text-left"
-        @mousedown="$emit('selectStreamer', streamer)">
-        {{ streamer.uname }} (房间号: {{ streamer.roomid }})
-      </button>
+    <div v-show="is_searchbar_focused" class="absolute w-full shadow-md rounded p-2 bg-white">
+      <div v-if="isLoading" class="animate-pulse bg-slate-200 h-4 w-24 my-1 mx-2 rounded"></div>
+      <div v-else class="flex flex-col">
+        <span class="pl-3">
+          "{{ results_pattern }}" 搜索结果
+        </span>
+        <button v-for="streamer in search_results" :key="streamer.uid"
+          class="p-2 pl-3 rounded hover:bg-gray-200 text-left" @mousedown="$emit('selectStreamer', streamer)">
+          {{ streamer.uname }} (房间号: {{ streamer.roomid }})
+        </button>
+      </div>
     </div>
   </div>
 
-  <div class="mx-10 my-8 mb-24">
+  <div class="mx-10 my-8 mb-20">
     <slot />
   </div>
   <!-- AppBody -->
