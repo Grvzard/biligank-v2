9e08b27d3d446acdb983c73f3132c8b106c5a564
grvzard <grvzard@outlook.com>
2024-04-10 16:38:43 +0800

    frontend: add ads

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,9 +1,13 @@
 import axios from 'axios'
 
 console.log(`running with backend: ${import.meta.env.VITE_BACKEND_URL}`)
-const service = axios.create({
+const apiMain = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 6000,
 })
+export const apiAds = axios.create({
+  baseURL: import.meta.env.VITE_ADS_BACKEND,
+  timeout: 6000,
+})
 
-export default service
+export default apiMain
